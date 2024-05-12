6df7e2f172c1dfb0c1cb913b06f97c0ed8d7c0bb
AdamTech <147929248+AdamKhelladi@users.noreply.github.com>
2024-05-12 18:46:56 +0100

    Update main.js
    Make Background URL Changing

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 // Pixels Tech Website
 
+// Make Background URL Changing 
+
+let landingPage = document.querySelector(".landing-page");
+let imagesArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+
+setInterval(() => changeBackgroundUrl(), 5000);
+
+function changeBackgroundUrl() {
+  let randomImgage =
+    imagesArray[Math.floor(Math.random() * imagesArray.length)];
+  landingPage.style.backgroundImage = `url(../media/${randomImgage})`;
+}
+
